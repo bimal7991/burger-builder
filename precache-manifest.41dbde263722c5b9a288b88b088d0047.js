d8c5d32a9836c1ee1dc6dca8a4f189ab276325aa
Bimal Paul <bhire1997@gmail.com>
2020-04-28 10:00:16 +0530

    Updates

---
+++ precache-manifest.41dbde263722c5b9a288b88b088d0047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef853b51ad1f08faccee6c3790270f1a",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "f8d9d559411b49c67dea",
+    "url": "/burger-builder/static/js/2.d4724681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.d4724681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a710ef1adf2504d5b9eb",
+    "url": "/burger-builder/static/js/main.9dd2b6e1.chunk.js"
+  },
+  {
+    "revision": "aa51b73ea7754a5431dd",
+    "url": "/burger-builder/static/js/runtime-main.27f48eea.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/Logo.b8503d26.png"
+  }
+]);
